b952408da2e11b72fa32cbb7f91f78ac488ee999
dev0cloo <etor.ocloo@gmail.com>
2023-01-08 11:33:39 +0000

    Add HeaderSocials.jsx and Edit Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import './Header.css'
+import React from "react";
+import "./Header.css";
+import CTA from "../CTA/CTA";
+import ME from "../../assets/perfil.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I am</h5>
+        <h1>Etornam Ocloo</h1>
+        <h5 className="text-light">Full Stack Developer</h5>
+        <CTA />
 
-export default Header
+        <div className="me">
+          <img src={ME} alt="an illustration of me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+
+        <HeaderSocials/>  
+      </div>
+    </header>
+  );
+};
+
+export default Header;
