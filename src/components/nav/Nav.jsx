e9b8ba44aa03a3ba8bd33902e47bea920a343a12
dev0cloo <etor.ocloo@gmail.com>
2023-01-08 14:48:32 +0000

    Edit Nav.jsx

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './Nav.css'
+import React from "react";
+import "./Nav.css";
+import { AiOutlineHome, AiOutlineProject } from "react-icons/ai";
+import { IoSchoolOutline } from "react-icons/io5";
+import { GrContact, GrProjects } from "react-icons/gr";
+import { FiSettings } from "react-icons/fi";
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <nav>
+      <ul>
+        <a href="#home">
+          <AiOutlineHome />
+        </a>
+        <a href="#about">
+          <IoSchoolOutline />
+        </a>
+        <a href="">
+          <AiOutlineProject />
+        </a>
+        <a href="#portfolio">
+          <GrProjects />
+        </a>
+        <a href="#contact">
+          <GrContact />
+        </a>
+        <a href="#"><FiSettings/></a>
+      </ul>
+    </nav>
+  );
+};
 
-export default Nav
+export default Nav;
