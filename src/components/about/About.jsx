3dfceadb4970b255a6e7d72d31bd18a029ef19c5
dev0cloo <etor.ocloo@gmail.com>
2023-01-09 15:24:39 +0000

    Update About Section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,50 +8,49 @@ import { VscFolderLibrary } from "react-icons/vsc";
 const About = () => {
   return (
     <section id="about">
-        <h5>Get To Know</h5>
-        <h2>About Me</h2>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
       <div className="container about__container">
-
-        <div className="about_container_main">
-          <div className="about__image">
+        <div className="about_me">
+          <div className="about_me_image">
             <img src={ABOUT} alt="me" />
           </div>
-          <div className="about__content">
-            <div className="card">
-              <div className="about__card">
-                <a className="about_link" href="#experience">
-                  <RiMedalLine />
-                  <h3>Experience</h3>
-                  <h5>2+ Years Working</h5>
-                </a>
-              </div>
-              <div className="about__card">
-                <a className="about_link" href="#testimonials">
-                  <BsPeople />
-                  <h3>Clients</h3>
-                  <h5>50+ Worldwide</h5>
-                </a>
-              </div>
-              <div className="about__card">
-                <a className="about_link" href="#portfolio">
-                  <VscFolderLibrary />
-                  <h3>Projects</h3>
-                  <h5>8+ Completed</h5>
-                </a>
-              </div>
-            </div>
-            <div>
-              <p className="about__me">
-                Hi there! I'm full stack developer with a love for all things
-                tech and a passion for creating innovative web solutions. With a
-                strong foundation in front-end and back-end technologies and
-                experience working with smart contracts on Ethereum and Solidity
-                projects, I'm always eager to push the boundaries of what's
-                possible.about
-              </p>
-            </div>
-            <Talk />
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <a className="about_link" href="#experience">
+              <article className="about__card">
+                <RiMedalLine className="about_card_icon" />
+                <h5>Experience</h5>
+                <small>2+ Years Working</small>
+              </article>
+            </a>
+            <a className="about_link" href="#testimonials">
+              <article className="about__card">
+                <BsPeople className="about_card_icon" />
+                <h5>Clients</h5>
+                <small>50+ Worldwide</small>
+              </article>
+            </a>
+            <a className="about_link" href="#portfolio">
+              <article className="about__card">
+                <VscFolderLibrary className="about_card_icon" />
+                <h5>Projects</h5>
+                <small>20+ Completed</small>
+              </article>
+            </a>
           </div>
+          <div>
+            <p className="about__me">
+              Hi there! I'm full stack developer with a love for all things tech
+              and a passion for creating innovative web solutions. With a strong
+              foundation in front-end and back-end technologies and experience
+              working with smart contracts on Ethereum and Solidity projects,
+              I'm always eager to push the boundaries of what's possible.about
+            </p>
+          </div>
+          <Talk />
         </div>
       </div>
     </section>
